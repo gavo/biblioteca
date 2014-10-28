55d53936090bb246056326a0864a89ec2afc8b25
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-10-28 00:40:00 -0400

    Commit inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<title>Biblioteca Virtual
+</title>
+</head>
+<body>
+
+</body>
+</html>
